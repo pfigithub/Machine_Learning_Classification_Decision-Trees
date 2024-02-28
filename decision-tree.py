89d5e523adcfd4563b7bf69ea1fdd82d785766f9
pfigithub <pampam7072@gmail.com>
2024-02-28 14:27:53 +0330

    sepration

--- decision-tree.py
+++ decision-tree.py
@@ -6,3 +6,7 @@ from sklearn.tree import DecisionTreeClassifier
 # reading data
 my_data = pd.read_csv("drug200.csv", delimiter=",")
 my_data[0:5]
+
+# seprating x and y(base on our data)
+X = my_data[['Age', 'Sex', 'BP', 'Cholesterol', 'Na_to_K']].values
+y = my_data["Drug"]
