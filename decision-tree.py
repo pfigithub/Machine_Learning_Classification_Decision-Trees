2bb2a45d2140d0ffa342ddec8099005f27a09e45
pfigithub <pampam7072@gmail.com>
2024-02-28 14:28:46 +0330

    pre-processing

--- decision-tree.py
+++ decision-tree.py
@@ -10,3 +10,22 @@ my_data[0:5]
 # seprating x and y(base on our data)
 X = my_data[['Age', 'Sex', 'BP', 'Cholesterol', 'Na_to_K']].values
 y = my_data["Drug"]
+
+# pre-processing
+from sklearn import preprocessing
+le_sex = preprocessing.LabelEncoder()
+le_sex.fit(['F','M'])
+X[:,1] = le_sex.transform(X[:,1]) 
+
+
+le_BP = preprocessing.LabelEncoder()
+le_BP.fit([ 'LOW', 'NORMAL', 'HIGH'])
+X[:,2] = le_BP.transform(X[:,2])
+
+
+le_Chol = preprocessing.LabelEncoder()
+le_Chol.fit([ 'NORMAL', 'HIGH'])
+X[:,3] = le_Chol.transform(X[:,3]) 
+
+X[0:5]
+
